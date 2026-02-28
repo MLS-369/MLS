7f68d4fb0bdf8ab166f28c6dbc4791e610b1b7e8
MLS-369 <nithin041406@gmail.com>
2026-02-28 10:16:02 +0530

    Update CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://your-frontend-name.vercel.app"
+    "https://mls-frontend-nu.vercel.app/"
   ],
   credentials: true
 }));
